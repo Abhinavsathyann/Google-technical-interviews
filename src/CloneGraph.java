9120a774a829811559c062fd17f18db0b573eaf0
Abhinav K <abhinavksathyann@gmail.com>
2025-10-29 21:54:16 +0530

    new file:   src/CloneGraph.java

---
+++ src/CloneGraph.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class CloneGraph {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+        if (visited.containsKey(node)) return visited.get(node);
+
+        Node clone = new Node(node.val);
+        visited.put(node, clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
+    }
+
+    public static void main(String[] args) {
+        // Example: Creating a simple graph and cloning it
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        node1.neighbors.add(node2);
+        node2.neighbors.add(node1);
+
+        CloneGraph cg = new CloneGraph();
+        Node cloned = cg.cloneGraph(node1);
+        System.out.println("Cloned graph node value: " + cloned.val);
+    }
+}
